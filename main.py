32f87e1985adc47e1cfd339f9dfea57ad03c5852
dimakiss <40802862+dimakiss@users.noreply.github.com>
2020-10-29 02:19:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
-from datetime import datetime
+import time,sys,random
+
+
 
 def isLoadedByXpath(driver,xpath):
     try:
@@ -17,66 +17,70 @@ def deleteByXpath(driver,xpath):
         return delelement.size["height"]
     except:
         return 0
-def get_screen_shot(driver,url):
+def search(driver, url,text_for_wolfram_alpha,image_name):
     driver.get(url)
 
     inputElement = driver.find_element_by_xpath('//*[@id="__next"]/div/div/div/div/div[1]/section/form/div/div/input')
     inputElement.send_keys(text_for_wolfram_alpha)
     inputElement.send_keys(Keys.ENTER)
 
-    # while (isLoadedByXpath('//*[@id="__next"]/div/div/main/div[2]/div/div/section/header/h2'))==False:
-    #   pass
-    # driver.save_screenshot("sc_"+str(count)+".png")
-    while (
-    isLoadedByXpath(driver,'//*[@id="__next"]/div/div/main/div[2]/div/div/section/header')) != True and isLoadedByXpath(
-            driver,'//*[@id="__next"]/div/div/main/div[2]/div/div/section') != True:
+    while (isLoadedByXpath(driver,'//*[@id="__next"]/div/div/main/div[2]/div/div/section/header')) != \
+            True and isLoadedByXpath(driver,'//*[@id="__next"]/div/div/main/div[2]/div/div/section') != True:
         pass
-    time.sleep(10)
+    time.sleep(15) # the time you give the search engine to load
     size=0
     size+=deleteByXpath(driver, '//*[@id="__next"]/div/div/main/a')
     size+=deleteByXpath(driver, '//*[@id="__next"]/div/header')
-
     size+=deleteByXpath(driver,'//*[@id="__next"]/div/div/main/div[2]/div/div/div[1]/section/div/div')
     size+=deleteByXpath(driver,'//*[@id="__next"]/div/div/main/div[2]/div/div/section')
     size+=deleteByXpath(driver,'//*[@id="__next"]/div/div/main/div[2]/div/div/div[2]')
     size+=deleteByXpath(driver, '//*[@id="__next"]/div/footer')
     size+=deleteByXpath(driver, '//*[@id="__next"]/div/section')
-
-
-    # the element with longest height on page
     time.sleep(1)
+    # the element with longest height on page
     ele = driver.find_element("xpath", '//*[@id="__next"]/div/div/main')
-    total_height = ele.size["height"]-size
-    driver.set_window_size(800, total_height)  # the trick
+    total_height = min(ele.size["height"]-size,2160) # You can change the default max height which is 2160
+    #driver.set_window_size(1280 , wanted_height)  # the trick
+    driver.set_window_size(1280,total_height)  # the trick
     if driver.find_elements_by_xpath('//*[@id="__next"]/div/div/div/div/img')!=[] or driver.find_elements_by_xpath("//*[contains(text(), 'Try another server')]")!=[]:
         driver.back()
         time.sleep(5)
     if driver.find_elements_by_xpath('//*[@id="__next"]/div/div/div/div/img')!=[] or driver.find_elements_by_xpath("//*[contains(text(), 'Try another server')]")!=[]:
         raise
-    driver.save_screenshot("sc_"+str(count)+".png")
-    #driver.close()
-
-count=1
-text_for_wolfram_alpha="integral of tan(x^2)"
+    driver.save_screenshot(image_name)
+    driver.close()
 
 
-while(True):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument("--test-type")
-    options.add_argument("--headless")
-    options.add_argument("--hide-scrollbars")
-    options.add_argument("--disable-web-security")
-    driver = webdriver.Chrome(chrome_options=options)
-    try:
-        s=datetime.now().second
-        m = datetime.now().minute
-        h=datetime.now().hour
-        count=m*60+s
-        get_screen_shot(driver,'https://www.wolframalpha.com')
-        break
-    except:
-        driver.close()
-        pass
 
+def create_wolf_screen_shot(text_for_wolfram_alpha,image_name):
+    tries=0
+    while(tries<3):
+        options = webdriver.ChromeOptions() #you can add the path here if its not in the same folder as the script
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument("--test-type")
+        options.add_argument("--headless")
+        options.add_experimental_option('excludeSwitches', ['enable-logging']) # disable printing
+        options.add_argument("--hide-scrollbars")
+        options.add_argument("--disable-web-security")
+        driver = webdriver.Chrome(options=options)
+        try:
+            search(driver, 'https://www.wolframalpha.com',text_for_wolfram_alpha,image_name)
+            break
+        except:
+            tries+=1
+            driver.close()
+            return False
+            pass
+    return True
+
+def gen_image_name():
+    return "wolf_sc_"+str(random.randint(10,1000))+".png"
+
+
+if __name__ == '__main__':
+    imag_name=gen_image_name()
+    wolf_text=" ".join(sys.argv[1:])
+    print("strat scraping you image will be save as "+imag_name)
+    create_wolf_screen_shot(wolf_text,imag_name)
+    exit()
 
